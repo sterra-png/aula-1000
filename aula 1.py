b7d00a7f7cd0bd1ad4397bd1b39b5ea16d70ad55
felipe <felipebaise@gmail.com>
2024-06-13 20:37:31 -0300

    first commit

---
+++ aula 1.py
@@ -0,0 +1,6 @@
+nome = "Paula Martins"
+mes = 'janeiro'
+valor = 500
+desconto = '10%'
+cupom = 'paulaé10'
+print("ola" , nome , "em" , mes ,"voce realizou uma compra de" , valor , "e ganhou um desconto de" , desconto ,"em sua proxima compra use o cupom" , cupom )
