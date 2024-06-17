68e9fddfe7d5a34c116f0268f811b0f418e70a37
felipe <felipebaise@gmail.com>
2024-06-17 20:57:00 -0300

    conta

---
+++ aula 2/aula 02.py
@@ -0,0 +1,12 @@
+numero = int(input('qual o valor ?'))
+dobro = numero * 2
+v2 = numero * 3
+v3 = numero ** 2
+v4 = numero ** (1/2)
+v5 = numero ** (1/3)
+
+print('o dobro do valor {}'.format(dobro))
+print('o triplo do valor {}'.format(v2))
+print('o quadroplo do valor {}'.format(v3))
+print('raiz quadrada do valor {}'.format(v4))
+print('raiz cubica do valor {}'.format(v5))
