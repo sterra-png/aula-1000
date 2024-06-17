b80c7c64687bff1c2ae35a6d30cfa4b468861e72
felipe <felipebaise@gmail.com>
2024-06-17 20:28:06 -0300

    atividade de aula 2

---
+++ aula 2/aula 2 .py
@@ -0,0 +1,10 @@
+nome = str(input('qual o seu nome'))
+p1 = float(input('Qual foi sua nota de português?'))
+m2 = float(input('Qual sua nota de matemática?'))
+c3 = float(input('Qual sua nota de ciências?'))
+b4 = float(input('Qual sua nota de biologia?'))
+
+media = (p1 + m2 + c3 + b4) / 4
+
+#print('sua media e',media)
+print('prazer {} sua media é {}'.format(nome,media))
