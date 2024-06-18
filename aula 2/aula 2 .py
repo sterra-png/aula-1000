e7efb3b89a14cbb2f20ced39cf53ade559fac850
felipe <felipebaise@gmail.com>
2024-06-17 21:13:27 -0300

    tru ou fake

--- aula 2/aula 2 .py
+++ aula 2/aula 2 .py
@@ -1,4 +1,5 @@
 nome = str(input('qual o seu nome'))
+
 p1 = float(input('Qual foi sua nota de português?'))
 m2 = float(input('Qual sua nota de matemática?'))
 c3 = float(input('Qual sua nota de ciências?'))
---
+++ aula 2/aula002.py
@@ -0,0 +1,6 @@
+#idade = str(input('digite sua cidade'))
+cidade = 'ilhabela'
+idade = 18
+
+if cidade == 'ilhabela' and idade >= 18:
+    print('certo')
