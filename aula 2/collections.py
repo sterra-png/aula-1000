9de929d8eac6cbb2eff8714c3e83995a18e689cf
sterra-png <felipebaise@gmail.com>
2024-06-18 01:13:34 -0300

    algumas colections

---
+++ aula 2/collections.py
@@ -0,0 +1,51 @@
+familia = ['maria','laura','marcelo','rodrigo','felipe']
+
+#             0       1        2        3         4
+#            -5      -4       -3       -2        -1
+#print(familia[1:]) #retorna apartir do indice 1
+#print(familia[-1]) #retorna um indice de traz para frente
+#print(familia[1]) #retorna um indice
+#print(familia[1:3]) #retorna o indice de 1 ate 4 sendo que o 4 e excluido
+
+#print(familia)
+#familia [4] = 'astoufo'#troca o nome de alguem na lista pelo nome que tu escolheu
+#print(familia)
+
+#familia.extend(['erminho','fabiana']) #adiciona uma x quantidaded de nomes para a lista, sempre mais de um
+#print(familia)
+
+#familia.append('felipe')#adiciona apenas mais um na lista
+#print(familia)
+
+#familia.insert(1,'pato') #adiciona 1 nome novo entre 0 e 2 que e 1
+#print(familia)
+
+#familia.pop() #remove o utimo indice 
+#print(familia)
+
+#familia.remove('rodrigo') #elimina o nome de alguem que vc escolha
+#print(familia)
+
+#print(familia)
+#familia.clear()
+#print(familia)
+
+#print(familia.index('maria')) #encontra onumero do index para vc
+#print(familia.count('maria')) #fala quantas vezes maria esta escrito na lista
+
+#idade_da_familia = ['20','32','2','12','6']
+#print(familia)
+#print(idade_da_familia)
+#idade_da_familia.sort() #ele faz a lista estar em forma alfabetica ou do maior para o menor do maior numero
+#familia.sort()
+#print(familia)
+#print(idade_da_familia)
+
+#print(familia)
+#familia.reverse() #inverte a posiçao da lista de traz para frente
+#print(familia)
+
+familia2 = familia.copy()
+familia.remove("felipe")
+print(familia)
+print(familia2)
